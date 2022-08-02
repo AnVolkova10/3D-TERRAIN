1413a1500281490dfaef324b714a4e8946b96aee
AnVolkova10 <curzi.angela@gmail.com>
2022-08-02 18:02:56 -0300

    added genoshanlike terrain & commented cat

--- src/script.js
+++ src/script.js
@@ -2,6 +2,17 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+// Loader
+
+const loader = new THREE.TextureLoader()
+const texture = loader.load('/textures/wireframe.png')
+const height = loader.load('/textures/height.png')
+const alpha = loader.load('/textures/alpha.jpg')
+
+const gltfloader = new GLTFLoader();
+
 
 // Debug
 const gui = new dat.GUI()
@@ -13,24 +24,54 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+
+// Cat
+// gltfloader.load( '/cat/scene.gltf', function ( gltf ) {
+// 	scene.add( gltf.scene );
+// }, undefined, function ( error ) {
+// 	console.error( error );
+// } );
+const geometry = new THREE.PlaneBufferGeometry(3, 3, 64, 64)
 
 // Materials
+const material = new THREE.MeshStandardMaterial({
+    color: 'gray',
+    map: texture,
+    displacementMap: height,
+    displacementScale: .5,
+    alphaMap: alpha,
+    transparent: true,
+    depthTest: false
+})
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+const plane = new THREE.Mesh(geometry, material)
+scene.add(plane)
+plane.rotation.x = 181
 
+gui.add(plane.rotation, 'x').min(0).max(300)
 // Mesh
-const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
 
 // Lights
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
+const pointLight = new THREE.PointLight(0xff4d71, 3)
+pointLight.position.x = .2
+pointLight.position.y = 10
+pointLight.position.z = 4.4
 scene.add(pointLight)
+
+gui.add(pointLight.position, 'y').min(-3).max(3).step(0.01)
+gui.add(pointLight.position, 'x').min(-6).max(10).step(0.01)
+gui.add(pointLight.position, 'z').min(-3).max(3).step(0.01)
+gui.add(pointLight, 'intensity').min(0).max(10).step(0.01)
+
+const lightColor = {
+    color: 0xd71948
+}
+
+gui.addColor(lightColor, 'color')
+    .onChange(()=> {
+        pointLight.color.set(lightColor.color)
+    })
 
 /**
  * Sizes
@@ -62,7 +103,7 @@ window.addEventListener('resize', () =>
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.x = 0
 camera.position.y = 0
-camera.position.z = 2
+camera.position.z = -2
 scene.add(camera)
 
 // Controls
@@ -73,7 +114,8 @@ scene.add(camera)
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    alpha: true
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -81,6 +123,12 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 /**
  * Animate
  */
+ const scrollMap = (event) => {
+    camera.position.z = window.scrollY / 20
+    
+}
+window.addEventListener('scroll', scrollMap);
+
 
 const clock = new THREE.Clock()
 
@@ -90,7 +138,9 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    sphere.rotation.y = .5 * elapsedTime
+
+    // plane.rotation.z = .5 * elapsedTime
+    // sphere.rotation.y = .5 * elapsedTime
 
     // Update Orbital Controls
     // controls.update()
@@ -103,3 +153,4 @@ const tick = () =>
 }
 
 tick()
+
